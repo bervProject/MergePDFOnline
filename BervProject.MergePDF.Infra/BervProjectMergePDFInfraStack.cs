13e3ad54e12cede263089b3a8f1cebbe58283e5d
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-04-10 16:18:11 +0700

    Temporary solution

---
+++ BervProject.MergePDF.Infra/BervProjectMergePDFInfraStack.cs
@@ -0,0 +1,57 @@
+using Amazon.CDK;
+using Amazon.CDK.AWS.Events;
+using Amazon.CDK.AWS.Events.Targets;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.Lambda;
+using Constructs;
+using System.IO;
+
+namespace BervProject.MergePDF.Infra
+{
+    public class BervProjectMergePDFInfraStack : Stack
+    {
+        internal BervProjectMergePDFInfraStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+
+            var role = Role.FromRoleName(this, "PDFMergeLambdaRole", "S3RoleLambda");
+
+            var buildOption = new BundlingOptions()
+            {
+                Image = Runtime.DOTNET_6.BundlingImage,
+                User = "root",
+                OutputType = BundlingOutput.ARCHIVED,
+                Command = new string[] {
+                   "/bin/sh",
+                    "-c",
+                    " dotnet tool install -g Amazon.Lambda.Tools"+
+                    " && dotnet build"+
+                    " && dotnet lambda package --output-package /asset-output/function.zip"
+                    }
+            };
+            var pdfMergerLambdaFunction = new Function(this, "PdfMerger", new FunctionProps
+            {
+                Runtime = Runtime.DOTNET_6,
+                Handler = "BervProject.MergePDF.Lambda::BervProject.MergePDF.Lambda.Functions_Default_Generated::Default",
+                Code = Code.FromAsset(Path.Combine(Directory.GetCurrentDirectory(), "BervProject.MergePDF.Lambda/src/BervProject.MergePDF.Lambda"), new Amazon.CDK.AWS.S3.Assets.AssetOptions
+                {
+                    Bundling = buildOption
+                }),
+                Role = role,
+            });
+            var rule = new Rule(this, "CronRule", new RuleProps
+            {
+                Schedule = Schedule.Cron(new CronOptions
+                {
+                    Minute = "0",
+                    Hour = "10",
+                    Day = "1",
+                    Month = "*",
+                    Year = "*",
+                }),
+            });
+            var lambdaFunctionTarget = new Amazon.CDK.AWS.Events.Targets.LambdaFunction(pdfMergerLambdaFunction, new LambdaFunctionProps());
+            rule.AddTarget(lambdaFunctionTarget);
+        }
+    }
+}
---
+++ BervProject.MergePDF.Lambda/src/BervProject.MergePDF.Lambda/Startup.cs
@@ -0,0 +1,38 @@
+using Amazon.S3;
+using BervProject.MergePDF.S3;
+using BervProject.MergePDF.S3.Models;
+
+namespace BervProject.MergePDF.Lambda
+{
+    [Amazon.Lambda.Annotations.LambdaStartup]
+    public class Startup
+    {
+        /// <summary>
+        /// Services for Lambda functions can be registered in the services dependency injection container in this method. 
+        ///
+        /// The services can be injected into the Lambda function through the containing type's constructor or as a
+        /// parameter in the Lambda function using the FromService attribute. Services injected for the constructor have
+        /// the lifetime of the Lambda compute container. Services injected as parameters are created within the scope
+        /// of the function invocation.
+        /// </summary>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder()
+                                        .AddJsonFile("appsettings.json")
+                                        .AddEnvironmentVariables();
+
+            //// Add AWS Systems Manager as a potential provider for the configuration. This is 
+            //// available with the Amazon.Extensions.Configuration.SystemsManager NuGet package.
+            builder.AddSystemsManager("/mergepdf/settings");
+
+            var configuration = builder.Build();
+            services.AddSingleton<IConfiguration>(configuration);
+            services.Configure<S3Settings>(configuration.GetSection("S3"));
+            services.AddAWSService<IAmazonS3>();
+            services.AddScoped<IDownloader, Downloader>();
+            services.AddScoped<IPdfMerger, PdfMerger>();
+            services.AddScoped<IUploader, Uploader>();
+            services.AddScoped<IMerger, Merger>();
+        }
+    }
+}
